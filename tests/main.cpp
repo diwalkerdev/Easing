13d279041b0a08e7a73e68abd4446a9a60f2d882
David Walker <dwalker0044@gmail.com>
2021-09-08 11:41:09 +0100

    New version of layout engine that allows items to be assigned dynamically and can be deleted.

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,9 +1,16 @@
-extern void debounce_main();
-extern void linear_main();
+extern void
+debounce_main();
+extern void
+linear_main();
+extern void
+Test_RemoveItems();
 
-int main()
+
+int
+main()
 {
     debounce_main();
     linear_main();
+    Test_RemoveItems();
     return 0;
 }
